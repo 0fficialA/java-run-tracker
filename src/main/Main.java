b41d4aae1541f6425c779be1ee6707ea90af40a1
0fficialA <a.opara@rocketmail.com>
2026-08-28 13:29:10 +0100

    Added a logic for main class which acts as a main entry for the program

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,11 @@
 package main;
 
+import ui.Login;
+
 //Application Entry
 public class Main{
+	public static void main(String[] args) {
+		//in the future entry should have login and sign up in the same form or a form with both uis embedded
+		Login login = new Login();
+	}
 }
